f863d65f650d32048f876c41c2e5299c98643c5b
Josh Fluke <jfluke717@gmail.com>
2015-12-06 11:26:56 -0700

    Latest work

--- Js/Angular/mainCtrl.js
+++ Js/Angular/mainCtrl.js
@@ -1,23 +1,66 @@
-var app = angular.module('workout', ['ngAnimate', 'ui.bootstrap','ui.router','firebase']);
+var app = angular.module('workout', ['ngAnimate', 'ui.bootstrap', 'ui.router', 'firebase']);
 
 
 app.controller('DropdownCtrl', function ($scope, fbService) {
 
-  $scope.status = {
-    isopen: false
-  };
-  
-  var comments = fbService.getAllComments()
-	$scope.comments = comments;
+	$scope.status = {
+		isopen: false
+	};
+
+	$scope.comments = fbService.getAllComments()  //firebase object
 	// comments.$bindTo($scope, 'comments')
 
-  $scope.commentText = "";
-console.log($scope.comments)
+ 
 
-$scope.addComment = function(){
-	console.log($scope.commentText)
-	comments.$add($scope.commentText)
+	$scope.addComment = function () {
+		console.log($scope.commentText)
+		$scope.comments.$add($scope.commentText)
+
+	}
+
+
+});
+
+
+app.controller('testctrl', function ($scope) {
+
+	$scope.hello = "enter a name"
+
+	$scope.x = true;
 	
-}
+	// $scope.change = function () {
+	// 	 debugger
+	// 	if($scope.random == '' && $scope.whatever.$dirty) {
+
+	// 		$scope.hello = "enter a name"
+	// 		$scope.showbtn = false
+
+	// 	}
+	// 	else {
+
+	// 		$scope.hello = 'hello ' + $scope.random
+	// 		$scope.showbtn = true
+	// 	}	
+	// }
+	
+	
+	$scope.calculate = function (){
+		var weight = $scope.weight * .45
+		var height = $scope.height * .025
+		
+		
+		$scope.results = weight /  Math.pow(height, 2) 
+		
+		
+		
+		
+		
+	}
+	
+
+
+
+	
+
 
 });
