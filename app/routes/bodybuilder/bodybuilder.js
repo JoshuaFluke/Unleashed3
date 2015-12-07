81f702f5c7b0c7399f4161f58d165042eed03524
Josh Fluke <jfluke717@gmail.com>
2015-12-06 18:54:53 -0700

    fix jquery

--- app/routes/bodybuilder/bodybuilder.js
+++ app/routes/bodybuilder/bodybuilder.js
@@ -58,4 +58,59 @@ angular.module('workout')
 
     $scope.results = weight /  Math.pow(height, 2)
   }
+
+
+  $(":checkbox[name='4[]']").change(function () {
+    if ($(":checkbox[name='4[]']:checked").length == 2)
+      $(':checkbox:not(:checked)').prop('disabled', true);
+    else
+      $(':checkbox:not(:checked)').prop('disabled', false);
+  })
+
+
+  $(".go").on('click', function () {
+
+    var arms = $("#arms").is(":checked")
+    var chest = $("#chest").is(":checked")
+    var legs = $("#legs").is(":checked")
+    var shoulders = $("#shoulders").is(":checked")
+    var back = $("#back").is(":checked")
+
+    //console.log('wegwegrwe')
+    if (arms) {
+      $(".arms").toggle('display')
+      $("#arms").hide();
+      $(".chkdesc").hide();
+    }
+    if (chest) {
+      $(".chest").toggle('display')
+      $("#chest").hide();
+      $(".chkdesc").hide();
+    }
+    if (legs) {
+      $(".legs").toggle('display')
+      $("#legs").hide();
+      $(".chkdesc").hide();
+    }
+    if (shoulders) {
+      $(".shoulders").toggle('display')
+      $("#shoulders").hide();
+      $(".chkdesc").hide();
+    }
+    if (back) {
+      $(".back").toggle('display')
+      $("#back").hide();
+      $(".chkdesc").hide();
+    }
+
+    $(this).prop("disabled", true)
+    $(this).css("display", "none");
+
+    $('.picker').text("Get ready to rock.")
+    $('.comments').css('display', 'block');
+
+    //    var age = $("#bs").val()
+
+    //    alert(age * 3);
+  });
 });
--- app/routes/go/go.js
+++ app/routes/go/go.js
@@ -6,10 +6,30 @@ angular.module('workout')
   $stateProvider
     .state('go', {
       url: '/go',
-      templateUrl: 'routes/go/go.html'
+      templateUrl: 'routes/go/go.html',
+      controller: 'GoCtrl'
     })
 })
 
 .controller('GoCtrl', function() {
+
+  $('.myButton').on('click', function (event) {
+    $('#content').toggle('display');
+    $('#content2').css('display', 'none')
+    $('#content3').css('display', 'none')
+  });
+
+
+  $('.myButton2').on('click', function (event) {
+    $('#content2').toggle('display')
+    $('#content3').css('display', 'none')
+    $('#content').css('display', 'none')
+  });
+
+  $('.myButton3').on('click', function (event) {
+    $('#content3').toggle('display')
+    $('#content2').css('display', 'none')
+    $('#content').css('display', 'none')
+  });
 
 });
