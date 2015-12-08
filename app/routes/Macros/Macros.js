eb25a29a9bffb6d9ab0491e027624706ebf08315
Josh Fluke <jfluke717@gmail.com>
2015-12-08 15:31:10 -0700

    New controller for routing, CSS updates

---
+++ app/routes/Macros/Macros.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Josh on 12/8/2015.
+ */
+angular.module('workout')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('macros', {
+        url: '/macros',
+        templateUrl: 'routes/Macros/Macros.html',
+        controller: 'macrosCtrl'
+      })
+  })
+
+.controller('macrosCtrl', function(){
+
+
+
+
+})
